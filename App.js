15c8f1e38019b149ed62b719d6ba9883a4619a92
VincentHuber <155532950+VincentHuber@users.noreply.github.com>
2024-03-05 10:07:04 +0100

    wip

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-import MapScreen from './screens/MapScreen';
-
->>>>>>> b1eddc93dbacd6cf987accc95492773b8be2bf6e
+import HomeScreen from './screens/HomeScreen';
 
 
 function App() {
   return (
-<<<<<<< HEAD
-    <View>
-      <EditProfile/>
-=======
     <View style={styles.container}>
-      <MapScreen/>
-
->>>>>>> b1eddc93dbacd6cf987accc95492773b8be2bf6e
+      <HomeScreen/>
     </View>
   );
 }
@@ -23,8 +14,6 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
