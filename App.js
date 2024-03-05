1e2cc269c16a53255de5ab9b0eb8203d48eb1742
Laure <lamechet4@gmail.com>
2024-03-05 09:42:56 +0100

    récupération app.Js

--- App.js
+++ App.js
@@ -1,31 +1,18 @@
 
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-import EditProfile from './screens/Editprofile'; 
-=======
-<<<<<<< HEAD
+import EditProfileScreen from './screens/editprofile'; 
 import MapScreen from './screens/MapScreen';
-=======
 import HomeScreen from './screens/HomeScreen';
-
->>>>>>> 12910ac4f4f110d4119a4f93a076a52cfdace41b
->>>>>>> b1eddc93dbacd6cf987accc95492773b8be2bf6e
-
 
 function App() {
   return (
-<<<<<<< HEAD
     <View>
-      <EditProfile/>
-=======
-    <View style={styles.container}>
-<<<<<<< HEAD
-      <MapScreen/>
 
-=======
-      <HomeScreen/>
->>>>>>> 12910ac4f4f110d4119a4f93a076a52cfdace41b
->>>>>>> b1eddc93dbacd6cf987accc95492773b8be2bf6e
+      <EditProfile/>
+    <View style={styles.container}>
+      <MapScreen/>
+      </View>
+
     </View>
   );
 }
@@ -33,11 +20,8 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
     alignItems: 'center',
     justifyContent: 'center',
-=======
->>>>>>> 12910ac4f4f110d4119a4f93a076a52cfdace41b
   },
 });
 
