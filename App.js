19b5046384a8b58bfa381708be7dd2ea8fe14cb4
Laure <lamechet4@gmail.com>
2024-03-06 12:30:54 +0100

    amélioration page EditProfil

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
@@ -6,19 +8,23 @@ import user from './reducers/users';
 
 import MapScreen from './screens/MapScreen';
 import HomeScreen from './screens/HomeScreen';
+import EditProfileScreen from './screens/EditProfileScreen';
 
+const Stack = createNativeStackNavigator();
 
 const store = configureStore({
   reducer: { user },
  });
 
 
-function App() {
+export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <HomeScreen/>
-      </View>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
     </Provider>
   );
 }
