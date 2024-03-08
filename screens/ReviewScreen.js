1b95b432f922cae478a9e9ee915cb677fc6a60bb
Feissoile <feissoileali@gmail.com>
2024-03-08 17:00:38 +0100

    test

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,236 +7,236 @@ import {
   Image,
   TextInput,
   SafeAreaView,
-  KeyboardAvoidingView,ScrollView,
-  Platform, 
-  StatusBar,Dimensions
-} from 'react-native';
-//import Icon from 'react-native-vector-icons/Ionicons';
-import Star from '../../Move-front/assets/star.js'
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+import Star from "../../Move-front/assets/star.js";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faStar as farStar, faStarHalf as farStarHalf, faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
 
 
-const BACKEND_ASSRESS='http://192.168.10.122:3000'
-
-
+const BACKEND_ASSRESS = "http://192.168.10.122:3000";
 
 export default function ReviewScreen() {
-
   const [isVisible, setIsVisible] = useState(false);
   const [ratingStars, setRatingStars] = useState(0);
-  const [reviewText, setReviewText] = useState('');
+  const [reviewText, setReviewText] = useState("");
   const [addReviews, setAddReviews] = useState([]);
   const [isTextInputVisible, setIsTextInputVisible] = useState(false);
   const [isReviewVisible, setIsReviewVisible] = useState(true);
 
-
   // cette fonction au clik fait disparaitre le bouton "laisser un avis"
   const toggleVisibility = () => {
-    setIsVisible(!isVisible)
+    setIsVisible(!isVisible);
     setIsReviewVisible(!isReviewVisible);
-  }; 
+  };
 
   const handleAddReview = () => {
+    // Vérifiez si le commentaire est vide
+    if (reviewText.trim() === "") {
+      console.error("Veuillez laisser un commentaire.");
+      return;
+    }
+    // la methode slice (...)
+    const trimmedReviewText = reviewText.slice(0, 250);
 
-  // Vérifiez si le commentaire est vide
-  if (reviewText.trim() === '') {
-    console.error('Le commentaire ne peut pas être vide.');
-    return;
-  }
-
-  const trimmedReviewText = reviewText.slice(0, 250);
-
-  // Vérifiez si la longueur du commentaire dépasse pas 250 caractères
+    // Vérifiez si la longueur du commentaire dépasse pas 250 caractères
+    //la propriété warn (...)
     if (reviewText.length > 250) {
-      console.warn('Vous avez dépasser le nombre de caractère autorisé.');
-      }
-  
-  // Si le commentaire n'est pas vide, continuez le traitement
+      console.warn("Vous avez dépasser le nombre de caractère autorisé.");
+    }
+
+    // Si le commentaire n'est pas vide, le traitement continue
     setIsVisible(false);
     setIsReviewVisible(true);
-    setReviewText('');
+    setReviewText("");
     setAddReviews([...addReviews, trimmedReviewText]);
 
+    console.log(reviewText);
 
-    console.log(reviewText)
-
-     // Envoi du commentaire au backend
+    // Envoi du commentaire au backend
     fetch(`${BACKEND_ASSRESS}/review/`, {
-      method : 'POST',
-      headers:{
-        'Content-Type': 'application/json',
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({review: reviewText}),
-  })
-  .then(response => response.json())
-  .then (data =>{
-    if (data.result){
-      console.log('Review ajouté avec succés');
-    }else{
-      console.error('Erreur lors de l\'ajout du review', data.error);
-    }
-  });
+      body: JSON.stringify({ review: reviewText }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          console.log("Review ajouté avec succés");
+        } else {
+          console.error("Erreur lors de l'ajout du review", data.error);
+        }
+      });
+  };
 
-};
+  const handleStarPress = (index) => {
+    setRatingStars(index + 1);
+  };
 
-const handleStarPress = (index) => {
-  setRatingStars(index + 1);
-};
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : null}
+      keyboardVerticalOffset={
+        Platform.OS === "ios" ? 64 : -StatusBar.currentHeight
+      }
+      style={{ flex: 1 }}
+    >
+      <SafeAreaView style={styles.container}>
+        <StatusBar
+          backgroundColor="transparent"
+          barStyle="dark-content"
+          translucent={true}
+        />
 
-
-
-return (<KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : null}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : -StatusBar.currentHeight}
-        style={{ flex: 1 }}
-      >
-  
-  <View style={styles.container}>
-   
-      <StatusBar backgroundColor="transparent" barStyle="dark-content" translucent={true}/>
-      
-     <View>
-      {isReviewVisible && (
-      <TouchableOpacity style={styles.button} onPress={toggleVisibility}>
-        <Text style={styles.text}>Laisser un avis</Text>
-        </TouchableOpacity>
-        )}
-        {isVisible  && (
-        <View style={styles.starWrapper}>
-            <View style={styles.starContainer}>
-           {[...Array(5)].map((_, index) => (
-                  <Star key={index} filled={index < ratingStars} onPress={() => handleStarPress(index)} />
-                ))}
+        <View>
+          {isReviewVisible && (
+            <TouchableOpacity style={styles.button} onPress={toggleVisibility}>
+              <Text style={styles.text}>Laisser un avis</Text>
+            </TouchableOpacity>
+          )}
+          {isVisible && (
+            <View
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <View style={styles.inputWrapper}>
+                <View>
+                  {[...Array(5)].map((_, index) => (
+                    <Star
+                      key={index}
+                      style={styles.buttonstars}
+                      filled={index < ratingStars}
+                      onPress={() => handleStarPress(index)}
+                    />
+                  ))}
                 </View>
+                <TextInput
+                  style={styles.input}
+                  multiline={true}
+                  placeholder="Votre avis"
+                  value={reviewText}
+                  onChangeText={setReviewText}
+                />
               </View>
-              )}
-               {isVisible && ( 
-                <View style={styles.inputWrapper}>
-            <TextInput
-              style={styles.input}
-              multiline={true}
-              placeholder="Votre avis"
-              value={reviewText}
-              onChangeText={setReviewText}
-              />
-              </View>)}
-              {isVisible && (
               <View style={styles.buttonreviewcontainer}>
-              <TouchableOpacity style={styles.buttonreview} onPress={handleAddReview}>
-                <Text style={styles.buttontextreview}>Deposer mon avis</Text>
-              </TouchableOpacity>
-              </View>)}
-              <View>
-                {addReviews.map((review, index) => (
-                <View key={index} style={styles.reviewItem}>
-                  <Text>{review}</Text>
-                </View>
-                ))}
-                </View>
-     </View>
-     </View>
-     </KeyboardAvoidingView>
-  
-        )
+                <TouchableOpacity
+                  style={styles.buttonreview}
+                  onPress={handleAddReview}
+                >
+                  <Text style={styles.buttontextreview}>Deposer mon avis</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
 
-    };
-
-    
-    
+          <View>
+            {addReviews.map((review, index) => (
+              <View key={index} style={styles.reviewItem}>
+                <Text>{review}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    color: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "blue",
+    alignItems: "center",
+    justifyContent: "center",
     //marginTop:100,
     //paddingTop: StatusBar.currentHeight || 0,
-    
   },
-  
+
   button: {
-    borderColor: '#4A46FF',
+    borderColor: "#4A46FF",
     borderWidth: 2,
     width: 317,
     height: 53,
-    //marginBottom: 300,
-    marginTop:200,
-    //marginLeft:100,
     paddingVertical: 12,
     borderRadius: 40,
-    alignItems: 'center'
+    alignItems: "center",
   },
   text: {
-    fontFamily: 'Poppins_600SemiBold',
+    fontFamily: "Poppins_600SemiBold",
     fontSize: 16,
-    textAlign: 'center',
-    color: '#4A46FF',
-    fontWeight: 'SemiBold',
+    textAlign: "center",
+    color: "#4A46FF",
+    fontWeight: "SemiBold",
     paddingTop: 4,
-  }, 
-
-  
+  },
 
   inputWrapper: {
-    backgroundColor:"black",
+    backgroundColor: "white",
     width: 317,
-    height:350,
-    borderRadius: 20, 
-    overflow: 'hidden',
-    marginTop:100, 
-    marginLeft:340,
-    marginBottom:10,
-    maxHeight: Dimensions.get('window').height - (StatusBar.currentHeight || 0) - 100
+    height: 350,
+    borderRadius: 20,
+    overflow: "hidden",
+    maxHeight:
+      Dimensions.get("window").height - (StatusBar.currentHeight || 0) - 100,
   },
 
-  starContainer:{
-    flexDirection: 'row',
-    width:1000,
+  starWrapper: {
+    color: "red",
+  },
+
+  starContainer: {
+    flexDirection: "row",
+    width: 100,
     marginBottom: 0,
-    paddingLeft:125,
-    paddingTop:100,
-    alignItems: 'center',
-
-
+    paddingLeft: 460,
+    paddingTop: 100,
+    //justifyContent:'center',
+    alignItems: "center",
+    color: "red",
   },
-  
-   
+
+  buttonstars: {
+    backgroundColor: "yellow",
+    height: 20,
+    width: 20,
+  },
 
   input: {
-    
-    backgroundColor:'#F4F4F4',
-    //color :'black',
+    backgroundColor: "#F4F4F4",
     height: 180,
-    marginBottom:50,
-    marginTop:150,
+    marginBottom: 50,
+    marginTop: 100,
     width: 300,
-    marginLeft:8,
-    paddingLeft:10,
+    marginLeft: 8,
+    paddingTop:20,
     borderRadius: 20,
     //justifyContent: 'flex-start',
     //alignItems: 'center',
-    
   },
-  
-  buttonreview:{
-    fontFamily: 'Poppins_600SemiBold',
-    backgroundColor:'#4A46FF',
+
+  buttonreview: {
+    fontFamily: "Poppins_600SemiBold",
+    backgroundColor: "#4A46FF",
     width: 317,
     height: 53,
-    marginLeft:340,
-    //marginBottom:5000,
     paddingVertical: 12,
-    //paddingtop:100,
-    //paddingLeft:0,
     borderRadius: 20,
-    alignItems: 'center'
-
+    alignItems: "center",
+    marginTop: 20,
   },
 
-  buttontextreview:{
-    color: 'white',
+  buttontextreview: {
+    color: "white",
     paddingTop: 5,
-    
   },
 
   /*reviewList: {
@@ -247,29 +247,14 @@ const styles = StyleSheet.create({
   },*/
 
   reviewItem: {
-    backgroundColor: 'yellow',
+    backgroundColor: "white",
+    //height:173,
     padding: 10,
     borderRadius: 20,
-    marginTop:20,
-    //marginBottom: 10,
+    marginTop: 20,
+    marginBottom: 1,
   },
-
 });
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
 
 /*const renderStars = () => {
   const stars = [];
@@ -285,4 +270,3 @@ const styles = StyleSheet.create({
     );
   }
   return stars };*/
-  
